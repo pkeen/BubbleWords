6715578134a27b93d4148f5cef168881269aa621
pkeen <pkeen7@gmail.com>
2023-07-06 11:38:19 -0600

    basic canvas graphics

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
 import Game from "./modules/game.js";
-import Display from "./modules/display.js";
 
 const appElement = document.getElementById('app');
 
-// const display = new Display(appElement);
-
 const game = new Game(appElement);
-
 
 game.init();
 
